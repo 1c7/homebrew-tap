f8a204b76412021badf0a10a09f9f370146f4a27
郑诚 (Cheng Zheng) <chengzheng.github@gmail.com>
2026-08-22 20:06:15 +0800

    feat: dynamic localized menubar text (最大用量 / MaxUsage)

--- Casks/max-usage.rb
+++ Casks/max-usage.rb
@@ -1,6 +1,6 @@
 cask "max-usage" do
   version "1.0.0"
-  sha256 "fe884178d72601858d4692a4609e04a79939d03a52f52a1d1739c194d57d7c10"
+  sha256 "14b744f35d3149434b846784f7c0cba3bcbfa2de68ff0faabeaa0a6bac33bcd6"
 
   url "https://github.com/1c7/max-usage/releases/download/v#{version}/MaxUsage-#{version}.dmg"
   name "MaxUsage"
