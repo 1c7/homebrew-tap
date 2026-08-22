355336bf6981d7a83468644b1fd40fcfea2654c9
郑诚 (Cheng Zheng) <chengzheng.github@gmail.com>
2026-08-22 20:00:27 +0800

    fix: instant live language switching across all dashboard views

--- Casks/max-usage.rb
+++ Casks/max-usage.rb
@@ -1,6 +1,6 @@
 cask "max-usage" do
   version "1.0.0"
-  sha256 "325d70155126c0edaa9ae87227061b1d1fba71c248b0420a9c6424e25fd0e03b"
+  sha256 "fe884178d72601858d4692a4609e04a79939d03a52f52a1d1739c194d57d7c10"
 
   url "https://github.com/1c7/max-usage/releases/download/v#{version}/MaxUsage-#{version}.dmg"
   name "MaxUsage"
